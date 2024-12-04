cf39ecca11fef02cc1c9b39d497bf20a3a633c34
marshift <marshift@riseup.net>
2024-12-04 23:33:00 +0000

    2024/4: this was bugging me

--- src/2024/4.ts
+++ src/2024/4.ts
@@ -15,21 +15,26 @@ MXMXAXMASX
 
 const search = "XMAS".split("");
 
-// The argument order of this callback is a bit weird but it lets me not include the row in Part 2 :p
-function iterate(input: string, callback: (grid: string[][], x: number, y: number, row: string[]) => void) {
+type Row = string[];
+type Grid = Row[];
+
+interface IterationData {
+	grid: Grid;
+	row: Row;
+	x: number;
+	y: number;
+}
+
+function iterate(input: string, callback: (data: IterationData) => void) {
 	const grid = input.split("\n").map((r) => r.split(""));
-	grid.forEach((row, y) => {
-		row.forEach((_, x) => {
-			callback(grid, x, y, row);
-		});
-	});
+	grid.forEach((row, y) => row.forEach((_, x) => callback({ grid, row, x, y })));
 }
 
 export const part1: Part = (input) => {
 	const fullSearch = [search, search.toReversed()];
 	let count = 0;
 
-	iterate(input, (grid, x, y, row) => {
+	iterate(input, ({ grid, row, x, y }) => {
 		const matches = [
 			fullSearch.some((s) => s.every((char, idx) => row[x + idx] === char)),
 			fullSearch.some((s) => s.every((char, idx) => grid[y + idx]?.[x] === char)),
@@ -47,7 +52,7 @@ export const part2: Part = (input) => {
 	const fullSearch = [search.slice(1), search.slice(1).toReversed()];
 	let count = 0;
 
-	iterate(input, (grid, x, y) => {
+	iterate(input, ({ grid, x, y }) => {
 		if (
 			fullSearch.some((s) => s.every((char, idx) => grid[y + idx]?.[x + idx] === char))
 			&& fullSearch.some((s) => s.every((char, idx) => grid[y + idx]?.[x + 2 - idx] === char))
