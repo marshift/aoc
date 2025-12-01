6452f6b40012b50917cac55542f7b7919b1887a4
marshift <marshift@riseup.net>
2025-12-01 10:57:02 +0000

    meta: remove inline jsr dotenv import

--- src/shared/input.ts
+++ src/shared/input.ts
@@ -1,10 +1,10 @@
-import { load as loadEnv } from "jsr:@std/dotenv";
+import { load as loadEnv } from "@std/dotenv";
 
 const { SESSION_TOKEN } = await loadEnv();
-const AOC_URL = "https://adventofcode.com";
+const AOC_BASE_URL = "https://adventofcode.com";
 
 export const getInput = (year: number, day: number) =>
-	fetch(`${AOC_URL}/${year}/day/${day}/input`, {
+	fetch(`${AOC_BASE_URL}/${year}/day/${day}/input`, {
 		headers: {
 			Cookie: `session=${SESSION_TOKEN}`,
 		},
