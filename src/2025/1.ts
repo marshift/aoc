8308569be80f27115ed3b2846cd79f2b2662839c
marshift <marshift@riseup.net>
2025-12-03 05:43:02 +0000

    2025/1: part 2

--- src/2025/1.ts
+++ src/2025/1.ts
@@ -13,11 +13,6 @@ R14
 L82
 `.trim();
 
-interface Rotation {
-	direction: "L" | "R";
-	distance: number;
-}
-
 const parse = (input: string) =>
 	input.split("\n").map((line) => {
 		const chars = line.split("");
@@ -27,23 +22,56 @@ const parse = (input: string) =>
 		} as Rotation;
 	});
 
+interface Rotation {
+	direction: "L" | "R";
+	distance: number;
+}
+
+class Dial {
+	#position = 50;
+	get position() {
+		return this.#position;
+	}
+
+	rotate(rot: Rotation) {
+		if (rot.direction === "L") this.#position -= rot.distance;
+		else this.#position += rot.distance;
+		return this.#position;
+	}
+}
+
 export const part1: Part = (input) => {
 	const rotations = parse(input);
-	let dial = 50;
+	const dial = new Dial();
+
 	let hits = 0;
-
-	for (const { direction, distance } of rotations) {
-		if (direction === "L") {
-			dial -= distance;
-		} else {
-			dial += distance;
-		}
-
-		if (dial % 100 === 0) hits++;
-	}
+	for (const rot of rotations) if (dial.rotate(rot) % 100 === 0) hits++;
 
 	return hits;
 };
 
 export const part2: Part = (input) => {
+	const rotations = parse(input);
+	const dial = new Dial();
+
+	let hits = 0;
+	for (const rot of rotations) {
+		let start, end;
+
+		const oldPos = dial.position;
+		const newPos = dial.rotate(rot);
+
+		if (rot.direction === "L") {
+			start = Math.ceil(newPos / 100);
+			end = Math.floor((oldPos - 1) / 100);
+		} else {
+			start = Math.ceil((oldPos + 1) / 100);
+			end = Math.floor(newPos / 100);
+		}
+
+		// I sure do love special cases not included in the example.
+		if (start <= end) hits += 1 + end - start;
+	}
+
+	return hits;
 };
