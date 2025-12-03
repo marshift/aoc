20c9049e18c25c9ab360bf26d3bc1edc8120ac67
marshift <marshift@riseup.net>
2025-12-03 21:56:27 +0000

    2025/3: cleanup and unify

--- src/2025/3.ts
+++ src/2025/3.ts
@@ -9,30 +9,14 @@ const testInput = `
 
 const parse = (input: string) => input.split("\n").map((line) => Array.from(line, Number));
 
-export const part1: Part = (input) => {
-	const banks = parse(input);
-
-	let joltage = 0;
-	for (const bank of banks) {
-		const tens = Math.max(...bank.slice(0, -1));
-		const ones = Math.max(...bank.slice(bank.indexOf(tens) + 1));
-
-		joltage += tens * 10 + ones;
-	}
-
-	return joltage;
-};
-
-export const part2: Part = (input) => {
-	const banks = parse(input);
-
+function getJoltage(banks: number[][], batteries: number) {
 	let joltage = 0;
 	for (const bank of banks) {
 		let final = 0;
 		let lastIdx = 0;
 
-		for (let n = 1; n <= 12; n++) {
-			const value = Math.max(...bank.slice(lastIdx, n === 12 ? undefined : n - 12));
+		for (let n = batteries; n >= 1; n--) {
+			const value = Math.max(...bank.slice(lastIdx, n === 1 ? undefined : -n + 1));
 			lastIdx = bank.indexOf(value, lastIdx) + 1;
 			final = (final * 10) + value;
 		}
@@ -41,4 +25,7 @@ export const part2: Part = (input) => {
 	}
 
 	return joltage;
-};
+}
+
+export const part1: Part = (input) => getJoltage(parse(input), 2);
+export const part2: Part = (input) => getJoltage(parse(input), 12);
