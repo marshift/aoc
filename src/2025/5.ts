98d6ffb839bd626dd4c86b09a9511bf5d306e0db
marshift <marshift@riseup.net>
2025-12-05 19:30:34 +0000

    2025/5: part 2

--- src/2025/5.ts
+++ src/2025/5.ts
@@ -14,12 +14,30 @@ const testInput = `
 32
 `.trim();
 
+function merge(ranges: number[][]) {
+	const sorted = ranges.toSorted((a, b) => a[0] - b[0]);
+	const merged = [];
+
+	let current = sorted.shift()!;
+	for (const next of sorted) {
+		if (current[1] < next[0]) {
+			merged.push(current);
+			current = next;
+		} else {
+			current[1] = Math.max(current[1], next[1]);
+		}
+	}
+	merged.push(current);
+
+	return merged;
+}
+
 function parse(input: string) {
 	const lines = input.split("\n");
 	const sepIdx = lines.findIndex((l) => l.length === 0);
 
 	return {
-		ranges: lines.slice(0, sepIdx).map((range) => range.split("-").map(Number)),
+		ranges: merge(lines.slice(0, sepIdx).map((range) => range.split("-").map(Number))),
 		ids: lines.slice(sepIdx + 1).map(Number),
 	};
 }
@@ -34,4 +52,10 @@ export const part1: Part = (input) => {
 };
 
 export const part2: Part = (input) => {
+	const { ranges } = parse(input);
+
+	let fresh = 0;
+	for (const [start, end] of ranges) fresh += (end - start) + 1;
+
+	return fresh;
 };
