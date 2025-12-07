02c62db27f563a0c6c9036099db9f4b608afd9a4
marshift <marshift@riseup.net>
2025-12-07 00:46:24 +0000

    2025/6: part 2

--- src/2025/6.ts
+++ src/2025/6.ts
@@ -1,18 +1,30 @@
 import type { Part } from "#shared/runner.ts";
 
-const testInput = `123 328  51 64
- 45 64  387 23
+const testInput = `123 328  51 64 
+ 45 64  387 23 
   6 98  215 314
 *   +   *   +  `;
 
-const parse = (input: string) => input.split("\n").map((row) => row.split(" ").filter((i) => i.length !== 0));
+const transpose = (grid: string[][]) => grid[0].map((_, i) => grid.map((row) => row[i]));
 
 export const part1: Part = (input) => {
-	const grid = parse(input);
-	const transposed = grid[0].map((_, i) => grid.map((row) => row[i]));
-	const problems = transposed.map((row) => row.join(row.pop()));
+	const grid = input.split("\n").map((row) => row.split(" ").filter((i) => i.length !== 0));
+	const problems = transpose(grid).map((row) => row.join(row.pop()));
 	return problems.map(eval).reduce((a, b) => a + b);
 };
 
 export const part2: Part = (input) => {
+	const chars = input.split("\n").map((line) => line.split(""));
+
+	const operators = chars.pop()!.filter((c) => c !== " ");
+	const transposed = transpose(chars);
+
+	const chunks = transposed.reduce((acc, curr) => {
+		if (curr.every((c) => c === " ")) acc.push([]);
+		else acc[acc.length - 1].unshift(curr.join("").trim());
+		return acc;
+	}, [[]] as string[][]);
+
+	const problems = chunks.map((digits, idx) => digits.join(operators[idx]));
+	return problems.map(eval).reduce((a, b) => a + b);
 };
