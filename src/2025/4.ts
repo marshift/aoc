a579711134c6d2fb1505cb22aabb423a8d8a2fde
marshift <marshift@riseup.net>
2025-12-07 01:16:39 +0000

    2025/4: part 2

--- src/2025/4.ts
+++ src/2025/4.ts
@@ -21,15 +21,13 @@ const parse = (input: string) =>
 		)
 	);
 
-export const part1: Part = (input) => {
-	const grid = parse(input);
+function processGrid(grid: boolean[][]) {
+	const /* jane */ removers = [];
 
-	let total = 0;
 	for (let y = 0; y < grid.length; y++) {
 		const row = grid[y];
 		for (let x = 0; x < row.length; x++) {
-			const col = row[x];
-			if (col !== true) continue; // No roll
+			if (row[x] !== true) continue; // No roll
 
 			if (
 				[
@@ -42,12 +40,26 @@ export const part1: Part = (input) => {
 					grid[y + 1]?.[x],
 					grid[y + 1]?.[x + 1],
 				].filter((v) => v === true).length < 4
-			) total++;
+			) removers.push(() => row[x] = false);
 		}
 	}
 
-	return total;
-};
+	return removers;
+}
+
+export const part1: Part = (input) => processGrid(parse(input)).length;
 
 export const part2: Part = (input) => {
+	const grid = parse(input);
+
+	let total = 0;
+	while (true) {
+		const removers = processGrid(grid);
+		if (removers.length === 0) break;
+
+		total += removers.length;
+		removers.forEach((r) => r());
+	}
+
+	return total;
 };
